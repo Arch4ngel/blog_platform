8dc1bdfdeaf2de3a69bf879b071d315cfe3b066d
Arch4ngel <mishacool4@gmail.com>
2024-03-13 02:48:02 +0300

    settings fix

--- config/settings.py
+++ config/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/code/staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -139,7 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # )
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = "/code/mediafiles"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 SMS_API_URL = os.environ.get('SMS_API_URL')
 
