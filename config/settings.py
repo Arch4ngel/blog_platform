e45a7f77bf904cdfc40a557f744a7608175d96da
Arch4ngel <mishacool4@gmail.com>
2024-03-14 01:14:12 +0300

    added gunicorn, nginx

--- config/settings.py
+++ config/settings.py
@@ -134,10 +134,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = (
-    BASE_DIR / 'static',
-)
-
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / "mediafiles"
 
