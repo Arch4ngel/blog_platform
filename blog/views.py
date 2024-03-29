bb8e6214ac2ec7553a866f7ddf80c5033afea95b
Arch4ngel <mishacool4@gmail.com>
2024-03-30 01:41:09 +0300

    fixed error 404

--- blog/views.py
+++ blog/views.py
@@ -54,7 +54,9 @@ class PostDetailView(DetailView):
     def dispatch(self, request, *args, **kwargs):
         """Проверка наличия подписки для платного поста"""
         obj = self.get_object()
-        if obj.is_private and not request.user.is_subscribed and not request.user.is_staff and request.user != obj.user:
+        if (request.user.is_anonymous and obj.is_private) or (obj.is_private and not request.user.is_subscribed
+                                                              and not request.user.is_staff
+                                                              and request.user != obj.user):
             return self.handle_no_permission()
         return super().dispatch(request, *args, **kwargs)
 
